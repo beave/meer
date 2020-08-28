6b49e8e065c83b2b6aed39147d2b8f002ba1ee20
Champ Clark III <champ@dev.quadrantsec.com>
2020-08-28 03:57:55 +0000

    Now only lookups fingerprint_network values.

--- src/fingerprint-to-json.c
+++ src/fingerprint-to-json.c
@@ -42,6 +42,8 @@ struct _Fingerprint_Networks *Fingerprint_Networks;
 
 struct _MeerConfig *MeerConfig;
 struct _MeerOutput *MeerOutput;
+struct _MeerCounters *MeerCounters;
+
 
 void Add_Fingerprint_To_JSON( struct json_object *json_obj, _DecodeAlert *DecodeAlert )
 {
@@ -50,13 +52,14 @@ void Add_Fingerprint_To_JSON( struct json_object *json_obj, _DecodeAlert *Decode
 
     int i = 0;
     int a = 0;
+    int z = 0;
 
     char fingerprint_tmp[PACKET_BUFFER_SIZE_DEFAULT] = { 0 };
     char tmp_command[PACKET_BUFFER_SIZE_DEFAULT] = { 0 };
     char tmp_new_alert[PACKET_BUFFER_SIZE_DEFAULT] = { 0 };
     char tmp_new_new_alert[PACKET_BUFFER_SIZE_DEFAULT] = { 0 };
 
-    unsigned char ip[MAXIPBIT] = { 0 };                                                                      
+    unsigned char ip[MAXIPBIT] = { 0 };
 
     struct json_object *json_obj_fingerprint = NULL;
     struct json_object *tmp = NULL;
@@ -64,6 +67,8 @@ void Add_Fingerprint_To_JSON( struct json_object *json_obj, _DecodeAlert *Decode
 
     char *tmp_ip = NULL;
     char *tmp_type = NULL;
+
+    bool valid_fingerprint_net = 0;
 
     char tmp_dhcp[1024] = { 0 };
 
@@ -75,31 +80,59 @@ void Add_Fingerprint_To_JSON( struct json_object *json_obj, _DecodeAlert *Decode
     for (a = 0; a < 2; a++ )
         {
 
+            valid_fingerprint_net = false;
+
             if ( a == 0 )
                 {
                     tmp_ip = DecodeAlert->src_ip;;
                     tmp_type = "src";
+
+                    IP2Bit(DecodeAlert->src_ip, ip);
+
+                    for ( z = 0; z < MeerCounters->fingerprint_network_count; z++ )
+                        {
+                            if ( Is_Inrange( ip, (unsigned char *)&Fingerprint_Networks[z].range, 1) )
+                                {
+                                    valid_fingerprint_net = true;
+                                }
+                        }
+
                 }
             else
                 {
                     tmp_ip = DecodeAlert->dest_ip;
                     tmp_type = "dest";
+
+                    IP2Bit(DecodeAlert->dest_ip, ip);
+
+                    for ( z = 0; z < MeerCounters->fingerprint_network_count; z++ )
+                        {
+                            if ( Is_Inrange( ip, (unsigned char *)&Fingerprint_Networks[z].range, 1) )
+                                {
+                                    valid_fingerprint_net = true;
+                                }
+                        }
                 }
 
-            snprintf(tmp_command, sizeof(tmp_command), "GET %s|dhcp|%s", FINGERPRINT_REDIS_KEY, tmp_ip);
-            Redis_Reader(tmp_command, tmp_dhcp, sizeof(tmp_dhcp));
 
-            if ( tmp_dhcp[0] != '\0' )
+            if ( valid_fingerprint_net == true )
                 {
 
-                    event_changed = true;
+                    snprintf(tmp_command, sizeof(tmp_command), "GET %s|dhcp|%s", FINGERPRINT_REDIS_KEY, tmp_ip);
+                    Redis_Reader(tmp_command, tmp_dhcp, sizeof(tmp_dhcp));
 
-                    strlcpy(tmp_new_alert, DecodeAlert->new_json_string, sizeof(tmp_new_alert));
-                    tmp_new_alert[ strlen(tmp_new_alert) - 2 ] = '\0';
+                    if ( tmp_dhcp[0] != '\0' )
+                        {
 
-                    snprintf(tmp_new_new_alert, sizeof(tmp_new_new_alert), "%s, \"fingerprint_dhcp_%s\": %s", tmp_new_alert, tmp_type, tmp_dhcp);
+                            event_changed = true;
 
-                    snprintf(DecodeAlert->new_json_string, sizeof(DecodeAlert->new_json_string), "%s }", tmp_new_new_alert);
+                            strlcpy(tmp_new_alert, DecodeAlert->new_json_string, sizeof(tmp_new_alert));
+                            tmp_new_alert[ strlen(tmp_new_alert) - 2 ] = '\0';
+
+                            snprintf(tmp_new_new_alert, sizeof(tmp_new_new_alert), "%s, \"fingerprint_dhcp_%s\": %s", tmp_new_alert, tmp_type, tmp_dhcp);
+
+                            snprintf(DecodeAlert->new_json_string, sizeof(DecodeAlert->new_json_string), "%s }", tmp_new_new_alert);
+                        }
                 }
         }
 
@@ -108,60 +141,86 @@ void Add_Fingerprint_To_JSON( struct json_object *json_obj, _DecodeAlert *Decode
     for (a = 0; a < 2; a++ )
         {
 
+            valid_fingerprint_net = false;
+
             if ( a == 0 )
                 {
                     tmp_ip = DecodeAlert->src_ip;
                     tmp_type = "src";
+
+                    IP2Bit(DecodeAlert->src_ip, ip);
+
+                    for ( z = 0; z < MeerCounters->fingerprint_network_count; z++ )
+                        {
+                            if ( Is_Inrange( ip, (unsigned char *)&Fingerprint_Networks[z].range, 1) )
+                                {
+                                    valid_fingerprint_net = true;
+                                }
+                        }
+
                 }
             else
                 {
                     tmp_ip = DecodeAlert->dest_ip;
                     tmp_type = "dest";
+
+                    IP2Bit(DecodeAlert->dest_ip, ip);
+
+                    for ( z = 0; z < MeerCounters->fingerprint_network_count; z++ )
+                        {
+                            if ( Is_Inrange( ip, (unsigned char *)&Fingerprint_Networks[z].range, 1) )
+                                {
+                                    valid_fingerprint_net = true;
+                                }
+                        }
+
                 }
 
-            reply = redisCommand(MeerOutput->c_redis, "SCAN 0 MATCH %s|event|%s|* count 1000000", FINGERPRINT_REDIS_KEY, tmp_ip);
-            key_count = reply->element[1]->elements;
 
-            if ( key_count > 0 )
+            if ( valid_fingerprint_net == true )
                 {
-                    event_changed = true;
-                    strlcpy(tmp_new_alert, DecodeAlert->new_json_string, sizeof(tmp_new_alert));
-                    tmp_new_alert[ strlen(tmp_new_alert) - 2 ] = '\0';
+
+                    reply = redisCommand(MeerOutput->c_redis, "SCAN 0 MATCH %s|event|%s|* count 1000000", FINGERPRINT_REDIS_KEY, tmp_ip);
+                    key_count = reply->element[1]->elements;
+
+                    if ( key_count > 0 )
+                        {
+                            event_changed = true;
+                            strlcpy(tmp_new_alert, DecodeAlert->new_json_string, sizeof(tmp_new_alert));
+                            tmp_new_alert[ strlen(tmp_new_alert) - 2 ] = '\0';
+                        }
+
+                    for ( i = 0; i < key_count; i++ )
+                        {
+                            redisReply *kr = reply->element[1]->element[i];
+                            snprintf(tmp_command, sizeof(tmp_command), "GET %s", kr->str);
+                            Redis_Reader(tmp_command, fingerprint_tmp, sizeof(fingerprint_tmp));
+
+                            if ( Validate_JSON_String( fingerprint_tmp ) == 0 )
+                                {
+                                    json_obj_fingerprint = json_tokener_parse(fingerprint_tmp);
+                                }
+                            else
+                                {
+                                    Meer_Log(WARN, "Incomplete or invalid fingerprint JSON for flow id %s", DecodeAlert->flowid);
+                                    continue;
+                                }
+
+                            if ( json_object_object_get_ex(json_obj_fingerprint, "fingerprint", &tmp))
+                                {
+                                    snprintf(tmp_new_new_alert, sizeof(tmp_new_new_alert), "%s, \"fingerprint_%s_%d\": %s", tmp_new_alert, tmp_type, i, json_object_get_string(tmp));
+                                    strlcpy(tmp_new_alert, tmp_new_new_alert, sizeof(tmp_new_alert));
+                                }
+                        }
                 }
-
-            for ( i = 0; i < key_count; i++ )
-                {
-                    redisReply *kr = reply->element[1]->element[i];
-                    snprintf(tmp_command, sizeof(tmp_command), "GET %s", kr->str);
-                    Redis_Reader(tmp_command, fingerprint_tmp, sizeof(fingerprint_tmp));
-
-                    if ( Validate_JSON_String( fingerprint_tmp ) == 0 )
-                        {
-                            json_obj_fingerprint = json_tokener_parse(fingerprint_tmp);
-                        }
-                    else
-                        {
-                            Meer_Log(WARN, "Incomplete or invalid fingerprint JSON for flow id %s", DecodeAlert->flowid);
-                            continue;
-                        }
-
-                    if ( json_object_object_get_ex(json_obj_fingerprint, "fingerprint", &tmp))
-                        {
-                            snprintf(tmp_new_new_alert, sizeof(tmp_new_new_alert), "%s, \"fingerprint_%s_%d\": %s", tmp_new_alert, tmp_type, i, json_object_get_string(tmp));
-                            strlcpy(tmp_new_alert, tmp_new_new_alert, sizeof(tmp_new_alert));
-                        }
-
-                }
-
         }
-
 
     /* Copy new JSON over,  if JSON has been updated */
 
     if ( event_changed == true )
         {
             /* Append final } */
-	
+
             snprintf(DecodeAlert->new_json_string, sizeof(DecodeAlert->new_json_string), "%s }", tmp_new_alert);
         }
 
